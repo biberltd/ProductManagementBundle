f8e58e642a700b5203335d54de5bf763514cf233
Said İmamoğlu <said.imamoglu@biberltd.com>
2014-09-09 15:39:15 +0300

    Bundle upgraded to 3.1

---
+++ Entity/AttributesOfProduct.php
@@ -0,0 +1,295 @@
+<?php
+/**
+ * @name        AttributesOfProduct
+ * @package		BiberLtd\Core\ProductManagementBundle
+ *
+ * @author      Can Berkol
+ * @author		Murat Ünal
+ *
+ * @version     1.0.1
+ * @date        30.11.2013
+ *
+ * @copyright   Biber Ltd. (http://www.biberltd.com)
+ * @license     GPL v3.0
+ *
+ * @description Model / Entity class.
+ *
+ */
+namespace BiberLtd\Core\Bundles\ProductManagementBundle\Entity;
+use Doctrine\ORM\Mapping AS ORM;
+use BiberLtd\Core\CoreEntity;
+
+/** 
+ * @ORM\Entity
+ * @ORM\Table(
+ *     name="attributes_of_product",
+ *     options={"charset":"utf8","collate":"utf8_turkish_ci","engine":"innodb"},
+ *     uniqueConstraints={@ORM\UniqueConstraint(name="idx_u_attributes_of_product", columns={"product","attribute"})}
+ * )
+ */
+class AttributesOfProduct extends CoreEntity
+{
+    /** 
+     * @ORM\Column(type="integer", length=10, nullable=false)
+     */
+    private $sort_order;
+
+    /** 
+     * @ORM\Column(type="datetime", nullable=false)
+     */
+    public $date_added;
+
+    /** 
+     * @ORM\Column(type="decimal", length=5, nullable=true)
+     */
+    private $price_factor;
+
+    /** 
+     * @ORM\Column(type="string", length=1, nullable=true)
+     */
+    private $price_factor_type;
+
+    /** 
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="BiberLtd\Core\Bundles\ProductManagementBundle\Entity\ProductAttribute")
+     * @ORM\JoinColumn(name="attribute", referencedColumnName="id", onDelete="CASCADE")
+     */
+    private $attribute;
+
+    /** 
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="BiberLtd\Core\Bundles\ProductManagementBundle\Entity\Product")
+     * @ORM\JoinColumn(name="product", referencedColumnName="id", onDelete="CASCADE")
+     */
+    private $product;
+
+    /**
+     * @name                  setAttribute ()
+     *                                     Sets the attribute property.
+     *                                     Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $attribute
+     *
+     * @return          object                $this
+     */
+    public function setAttribute($attribute) {
+        if(!$this->setModified('attribute', $attribute)->isModified()) {
+            return $this;
+        }
+		$this->attribute = $attribute;
+		return $this;
+    }
+
+    /**
+     * @name            getAttribute ()
+     *                               Returns the value of attribute property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->attribute
+     */
+    public function getAttribute() {
+        return $this->attribute;
+    }
+
+    /**
+     * @name                  setPriceFactor ()
+     *                                       Sets the price_factor property.
+     *                                       Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $price_factor
+     *
+     * @return          object                $this
+     */
+    public function setPriceFactor($price_factor) {
+        if(!$this->setModified('price_factor', $price_factor)->isModified()) {
+            return $this;
+        }
+		$this->price_factor = $price_factor;
+		return $this;
+    }
+
+    /**
+     * @name            getPriceFactor ()
+     *                                 Returns the value of price_factor property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->price_factor
+     */
+    public function getPriceFactor() {
+        return $this->price_factor;
+    }
+
+    /**
+     * @name                  setPriceFactorType ()
+     *                                           Sets the price_factor_type property.
+     *                                           Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $price_factor_type
+     *
+     * @return          object                $this
+     */
+    public function setPriceFactorType($price_factor_type) {
+        if(!$this->setModified('price_factor_type', $price_factor_type)->isModified()) {
+            return $this;
+        }
+		$this->price_factor_type = $price_factor_type;
+		return $this;
+    }
+
+    /**
+     * @name            getPriceFactorType ()
+     *                                     Returns the value of price_factor_type property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->price_factor_type
+     */
+    public function getPriceFactorType() {
+        return $this->price_factor_type;
+    }
+
+    /**
+     * @name                  setProduct ()
+     *                                   Sets the product property.
+     *                                   Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $product
+     *
+     * @return          object                $this
+     */
+    public function setProduct($product) {
+        if(!$this->setModified('product', $product)->isModified()) {
+            return $this;
+        }
+		$this->product = $product;
+		return $this;
+    }
+
+    /**
+     * @name            getProduct ()
+     *                             Returns the value of product property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->product
+     */
+    public function getProduct() {
+        return $this->product;
+    }
+
+    /**
+     * @name                  setSortOrder ()
+     *                                     Sets the sort_order property.
+     *                                     Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $sort_order
+     *
+     * @return          object                $this
+     */
+    public function setSortOrder($sort_order) {
+        if(!$this->setModified('sort_order', $sort_order)->isModified()) {
+            return $this;
+        }
+		$this->sort_order = $sort_order;
+		return $this;
+    }
+
+    /**
+     * @name            getSortOrder ()
+     *                               Returns the value of sort_order property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->sort_order
+     */
+    public function getSortOrder() {
+        return $this->sort_order;
+    }
+    /******************************************************************
+     * PUBLIC SET AND GET FUNCTIONS                                   *
+     ******************************************************************/
+
+}
+/**
+ * Change Log:
+ * **************************************
+ * v1.0.1                      Can Berkol
+ * 30.11.2013
+ * **************************************
+ * A getAttribute()
+ * A setAttribute
+ * D getProductAttribute()
+ * D setProductAttribute()
+ *
+ * **************************************
+ * v1.0.0                      Murat Ünal
+ * 11.09.2013
+ * **************************************
+ * A getDateAdded()
+ * A getPriceFactor()
+ * A getPriceFactorType()
+ * A getProduct()
+ * A getProductAttribute()
+ * A getSortOrder()
+ *
+ * A setDateAdded()
+ * A setPriceFactor()
+ * A setPriceFactorType()
+ * A setProduct()
+ * A setProduct()
+ * A setProductAttribute()
+ * A setSortOrder()
+ *
+ */
---
+++ Entity/VolumePricing.php
@@ -0,0 +1,297 @@
+<?php
+/**
+ * @name        VolumePricing
+ * @package		BiberLtd\Core\ProductManagementBundle
+ *
+ * @author		Can Berkol
+ *
+ * @version     1.0.0
+ * @date        22.05.2014
+ *
+ * @copyright   Biber Ltd. (http://www.biberltd.com)
+ * @license     GPL v3.0
+ *
+ * @description Model / Entity class.
+ *
+ */
+namespace BiberLtd\Core\Bundles\ProductManagementBundle\Entity;
+use Doctrine\ORM\Mapping AS ORM;
+use BiberLtd\Core\CoreEntity;
+/** 
+ * @ORM\Entity
+ * @ORM\Table(
+ *     name="volume_pricing",
+ *     options={"charset":"utf8","collate":"utf8_turkish_ci","engine":"innodb"},
+ *     indexes={
+ *         @ORM\Index(name="idx_n_volume_pricing", columns={"product"}),
+ *         @ORM\Index(name="idx_n_volume_pricing_date_added", columns={"date_added"}),
+ *         @ORM\Index(name="idx_n_volume_pricing_updated", columns={"date_updated"})
+ *     },
+ *     uniqueConstraints={@ORM\UniqueConstraint(name="idx_u_volume_pricing_id", columns={"id"})}
+ * )
+ */
+class VolumePricing extends CoreEntity
+{
+    /**
+     * @ORM\Column(type="integer", length=15)
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Id
+     */
+    private $id;
+    /** 
+     * @ORM\Column(type="integer", length=5, nullable=false)
+     */
+    private $quantity_limit;
+
+    /** 
+     * @ORM\Column(type="string", length=2, nullable=false)
+     */
+    private $limit_direction;
+
+    /** 
+     * @ORM\Column(type="datetime", nullable=false)
+     */
+    public $date_added;
+
+    /** 
+     * @ORM\Column(type="datetime", nullable=false)
+     */
+    public $date_updated;
+
+    /** 
+     * @ORM\Column(type="decimal", length=8, nullable=false)
+     */
+    private $price;
+
+    /** 
+     * @ORM\Column(type="decimal", length=8, nullable=true)
+     */
+    private $discounted_price;
+
+    /** 
+     * 
+     * @ORM\ManyToOne(targetEntity="BiberLtd\Core\Bundles\ProductManagementBundle\Entity\Product")
+     * @ORM\JoinColumn(name="product", referencedColumnName="id", nullable=false)
+     */
+    private $product;
+
+
+    /**
+     * @name                  setDiscountedPrice ()
+     *                                           Sets the discounted_price property.
+     *                                           Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $discounted_price
+     *
+     * @return          object                $this
+     */
+    public function setDiscountedPrice($discounted_price) {
+        if($this->setModified('discounted_price', $discounted_price)->isModified()) {
+            $this->discounted_price = $discounted_price;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @name            getDiscountedPrice ()
+     *                                     Returns the value of discounted_price property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->discounted_price
+     */
+    public function getDiscountedPrice() {
+        return $this->discounted_price;
+    }
+
+    /**
+     * @name                  setLimitDirection ()
+     *                                          Sets the limit_direction property.
+     *                                          Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $limit_direction
+     *
+     * @return          object                $this
+     */
+    public function setLimitDirection($limit_direction) {
+        if($this->setModified('limit_direction', $limit_direction)->isModified()) {
+            $this->limit_direction = $limit_direction;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @name            getLimitDirection ()
+     *                                    Returns the value of limit_direction property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->limit_direction
+     */
+    public function getLimitDirection() {
+        return $this->limit_direction;
+    }
+
+    /**
+     * @name                  setPrice ()
+     *                                 Sets the price property.
+     *                                 Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $price
+     *
+     * @return          object                $this
+     */
+    public function setPrice($price) {
+        if($this->setModified('price', $price)->isModified()) {
+            $this->price = $price;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @name            getPrice ()
+     *                           Returns the value of price property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->price
+     */
+    public function getPrice() {
+        return $this->price;
+    }
+
+    /**
+     * @name            getId ()
+     *                  Returns the value of id property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->pricing
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * @name                  setProduct ()
+     *                                   Sets the product property.
+     *                                   Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $product
+     *
+     * @return          object                $this
+     */
+    public function setProduct($product) {
+        if($this->setModified('product', $product)->isModified()) {
+            $this->product = $product;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @name            getProduct ()
+     *                             Returns the value of product property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->product
+     */
+    public function getProduct() {
+        return $this->product;
+    }
+
+    /**
+     * @name                  setQuantityLimit ()
+     *                                         Sets the quantity_limit property.
+     *                                         Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $quantity_limit
+     *
+     * @return          object                $this
+     */
+    public function setQuantityLimit($quantity_limit) {
+        if($this->setModified('quantity_limit', $quantity_limit)->isModified()) {
+            $this->quantity_limit = $quantity_limit;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @name            getQuantityLimit ()
+     *                                   Returns the value of quantity_limit property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->quantity_limit
+     */
+    public function getQuantityLimit() {
+        return $this->quantity_limit;
+    }
+
+}
+/**
+ * Change Log:
+ * **************************************
+ * v1.0.0                      Can Berkol
+ * 22.05.2014
+ * **************************************
+ * File Created
+ */
