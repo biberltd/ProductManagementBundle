04e641441a98c7095ca59d87cd0666fb4a7c674e
FURKAN KULABER <furkankulaber@clemury.com>
2020-10-08 17:28:51 +0300

    fix categories problem

--- Services/ProductManagementModel.php
+++ Services/ProductManagementModel.php
@@ -7111,6 +7111,7 @@ class ProductManagementModel extends CoreModel
         foreach ($resultSet as $anItem){
             $this->em->remove($anItem);
         }
+        $this->em->persist($anItem);
         $this->em->flush();
 
         return new ModelResponse(null, 0, 0, null, false, 'S:D:001', 'Selected entries have been successfully removed from database.', $timeStamp, microtime(true));
